d09454a50b9fd284219de668f021c2a939be70d8
Markus Hartig <markus.hartig@justrelate.com>
2023-08-07 13:18:02 +0200

    Test JrRestApi with IAM API key

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import Scrivito from "scrivito";
 
 Scrivito.configure({
   tenant: process.env.SCRIVITO_TENANT,
+  apiKey: {
+    clientId: process.env.IAM_CLIENT_ID,
+    clientSecret: process.env.IAM_CLIENT_SECRET,
+  },
 });
 
-Scrivito.load(() => Scrivito.Obj.onAllSites().all().count()).then((count) =>
-  console.log("count", count)
+const res = await Scrivito.load(() =>
+  Scrivito.unstable_JrRestApi.get("iam/userinfo")
 );
+
+console.log(res);
