8dfab9b1eb7f25e42e430d221575ffbdc28b45e0
Markus Hartig <markus.hartig@justrelate.com>
2024-05-23 17:12:50 +0200

    List basic user- and account info incl. team membership
    for configured tenant and API key

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 import "dotenv/config";
 import Scrivito from "scrivito";
 
+const tenant = process.env.SCRIVITO_TENANT;
+
 Scrivito.configure({
-  tenant: process.env.SCRIVITO_TENANT,
+  tenant: tenant,
+  apiKey: {
+    clientId: process.env.IAM_CLIENT_ID,
+    clientSecret: process.env.IAM_CLIENT_SECRET,
+  },
 });
 
-Scrivito.load(() => Scrivito.Obj.onAllSites().all().count()).then((count) =>
-  console.log("count", count)
-);
+const jrRestApi = Scrivito.createApiClient("https://api.justrelate.com");
+
+const userinfo = await jrRestApi.get(`iam/instances/${tenant}/userinfo`);
+const accountId = userinfo.aud[1];
+
+const [account, teams] = await Promise.all([
+  jrRestApi.get(`iam/accounts/${accountId}`),
+  jrRestApi.get(`iam/accounts/${accountId}/teams`),
+]);
+
+const teamNames = userinfo.team_ids.map((id) => teams.results.find((team) => team.id === id).name);
+
+console.log({
+  name: userinfo.name,
+  email: userinfo.email,
+  account_title: account.title,
+  account_id: accountId,
+  tenant_id: tenant,
+  teams: teamNames,
+});
